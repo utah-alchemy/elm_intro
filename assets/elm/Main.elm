464bf728c150d98e67e83517497329fca0cc19dc
Cody Poll <cjpoll@gmail.com>
2017-09-13 22:01:53 -0600

    Use Elm Architecture

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,7 +1,44 @@
 module Main exposing (..)
 
+import Html exposing (Html)
 import CoolApp.Page
 
 
+type Msg
+    = Noop
+
+
+type alias Model =
+    { name : String }
+
+
+main : Program Never Model Msg
 main =
+    Html.program
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { name = "" }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Noop ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
     CoolApp.Page.render
